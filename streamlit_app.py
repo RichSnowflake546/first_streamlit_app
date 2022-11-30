af59a22af976ef33a5d6ce6e6e77c14502d6fe58
RichSnowflake546 <119495471+RichSnowflake546@users.noreply.github.com>
2022-11-30 11:26:51 +0000

    Update streamlit_app.py
    fixed single quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title("My Parents New health Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text("Omega 3 & Blueberry Oatmeal")
 
-streamlit.text'Kale, Spinach & Rocket Smoothie')
+streamlit.text("Kale, Spinach & Rocket Smoothie")
