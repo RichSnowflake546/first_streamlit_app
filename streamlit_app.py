7e1890cc18fe9d1376d60a51069e18f500ef7fb2
RichSnowflake546 <119495471+RichSnowflake546@users.noreply.github.com>
2022-12-02 15:12:42 +0000

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from erllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title("My Parents New healthy Diner 2")
 
